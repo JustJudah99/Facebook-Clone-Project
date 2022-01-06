a87e905e94c193bcc67ca5424ae3b4bd7cdfabed
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-05 22:25:03 -0500

    feat(Client): Create InputPassword

--- client/src/components/Input/index.jsx
+++ client/src/components/Input/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './styles.css'
 
 const Input = ({ labelValue, ...props }) => {
@@ -9,4 +10,29 @@ const Input = ({ labelValue, ...props }) => {
   )
 }
 
-export { Input }
+const InputPassword = ({ labelValue, ...props }) => {
+  const [show, setShow] = useState(false)
+  return (
+    <div className="input">
+      {labelValue ?? <label className="input__label">{labelValue}</label>}
+      <div className="input__wrapper">
+        <input
+          className="input__element"
+          autoComplete="off"
+          spellCheck="false"
+          {...props}
+          type={show ? 'text' : 'password'}
+        />
+        <span className="input__btn-password" onClick={() => setShow((prev) => !prev)}>
+          {show ? (
+            <div className="input__password--icon-eye" />
+          ) : (
+            <div className="input__password--icon-eye-off" />
+          )}
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export { Input, InputPassword }
