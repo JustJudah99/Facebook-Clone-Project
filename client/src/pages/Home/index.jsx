cfac0e49b87f1569b8f8dd091b1189f92ca96a17
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-05 23:01:54 -0500

    feat(Client): Use LoginForm into HomePage

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,10 @@
+import LoginForm from '../../components/LoginForm'
 import './styles.css'
 
 const Home = () => {
   return (
     <div className="home__wrapper">
-      <h1 className="home__title">Facebook Clone</h1>
+      <LoginForm />
     </div>
   )
 }
