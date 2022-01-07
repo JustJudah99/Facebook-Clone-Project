4db520c9cf2ed33a2ecfbbd3976577f7b5bd3ca7
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-06 22:27:55 -0500

    fix(Client): Fix ButtonLink launch event submit

--- client/src/components/LoginForm/index.jsx
+++ client/src/components/LoginForm/index.jsx
@@ -4,7 +4,7 @@ import { Button, ButtonLink } from '../Button'
 import { Input, InputPassword } from '../Input'
 import './styles.css'
 
-const LoginForm = () => {
+const LoginForm = ({ handleSignUp }) => {
   const email = useField('email')
   const password = useField('password')
 
@@ -14,14 +14,16 @@ const LoginForm = () => {
         <Input {...email} placeholder="Correo electrónico o número de teléfono" />
         <InputPassword {...password} placeholder="Contraseña" />
         <div className="loginform__container">
-          <Button>Iniciar sesión</Button>
+          <Button type="submit">Iniciar sesión</Button>
         </div>
         <div className="loginform__container--margin-top">
           <Link to="/">¿Olvidaste tu contraseña?</Link>
         </div>
         <div className="loginform__separator" />
         <div className="loginform__container--less-padding">
-          <ButtonLink to="/">Crear cuenta nueva</ButtonLink>
+          <ButtonLink type="button" onClick={handleSignUp}>
+            Crear cuenta nueva
+          </ButtonLink>
         </div>
       </form>
     </div>
