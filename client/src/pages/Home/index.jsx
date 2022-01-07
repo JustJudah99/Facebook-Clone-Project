98b17181ae96dde2bd14553554b045793abef6ac
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-06 23:36:04 -0500

    feat(Client): Use Modal with register form

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react'
 import Footer from '../../components/Footer'
 import LoginForm from '../../components/LoginForm'
+import Modal from '../../components/Modal'
+import RegisterForm from '../../components/RegisterForm'
 import './styles.css'
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false)
   return (
     <div className="home__wrapper">
       <section className="home__section">
-        <LoginForm />
+        <LoginForm handleSignUp={() => setShowModal((prev) => !prev)} />
       </section>
+      {showModal && (
+        <Modal>
+          <RegisterForm />
+        </Modal>
+      )}
       <Footer />
     </div>
   )
