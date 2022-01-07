8324f6c2d363057efa76efe13e9819df977d76df
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-07 18:12:38 -0500

    feat(Client): Create component selectField and use into DateOfBirth

--- client/src/components/RegisterForm/components/DateOfBirthField.jsx
+++ client/src/components/RegisterForm/components/DateOfBirthField.jsx
@@ -1,39 +1,21 @@
+import { useState } from 'react'
+import { getDateSystem, getDays, getMonths, getYears } from '../../../utils'
 import ContainerField from './ContainerField'
+import SelectionField from './SelectionField'
 
 const DateOfBirthField = () => {
+  const [state, setState] = useState(getDateSystem())
+
+  const changeDay = ({ target }) => setState((prev) => ({ ...prev, day: target.value }))
+  const changeMonth = ({ target }) => setState((prev) => ({ ...prev, month: target.value }))
+  const changeYear = ({ target }) => setState((prev) => ({ ...prev, year: target.value }))
+
   return (
     <ContainerField label="Fecha de nacimiento">
       <div className="register-form__wrapper-select">
-        <select className="register-form__select">
-          <option value="1">1</option>
-          <option value="2">1</option>
-          <option value="3">1</option>
-          <option value="4">1</option>
-          <option value="5">1</option>
-          <option value="6">1</option>
-        </select>
-        <select className="register-form__select">
-          <option value="1">ene</option>
-          <option value="2">feb</option>
-          <option value="3">mar</option>
-          <option value="4">abr</option>
-          <option value="5">may</option>
-          <option value="6">jun</option>
-          <option value="7">jul</option>
-          <option value="8">ago</option>
-          <option value="9">sep</option>
-          <option value="10">oct</option>
-          <option value="11">nov</option>
-          <option value="12">dic</option>
-        </select>
-        <select className="register-form__select">
-          <option value="2022">2022</option>
-          <option value="2">1</option>
-          <option value="3">1</option>
-          <option value="4">1</option>
-          <option value="5">1</option>
-          <option value="6">1</option>
-        </select>
+        <SelectionField options={getDays()} value={state.day} onChange={changeDay} />
+        <SelectionField options={getMonths()} value={state.month} onChange={changeMonth} />
+        <SelectionField options={getYears()} value={state.year} onChange={changeYear} />
       </div>
     </ContainerField>
   )
