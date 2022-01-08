c0a63a84a963ad48cfb83a42b705cddca3d1ff2f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-08 16:17:50 -0500

    feat(Client): Add support errors only show no interactive

--- client/src/components/RegisterForm/components/InputField.jsx
+++ client/src/components/RegisterForm/components/InputField.jsx
@@ -1,9 +1,28 @@
-const InputField = ({ ...props }) => {
+const InputField = ({ state, setState, posError, messageError, regex, ...props }) => {
+  const handleChange = ({ target }) => setState((prev) => ({ ...prev, value: target.value }))
+  const validateField = () => {
+    if (!regex) return
+    const validated = regex.test(state.value)
+    setState((prev) => ({ ...prev, validated }))
+  }
+
   return (
     <div className="register-form__input-wrapper">
       <div className="register-form__input-container">
-        <input className="register-form__input-element" spellCheck="false" {...props} />
+        <input
+          {...props}
+          type={state.type}
+          value={state.value}
+          className={`register-form__input-element ${
+            state.validated === false ? 'register-form__error' : ''
+          }`}
+          spellCheck="false"
+          onChange={handleChange}
+          onKeyUp={validateField}
+          onBlur={validateField}
+        />
       </div>
+      {state.validated === false && <span className="register-form__input-element-icon" />}
     </div>
   )
 }
--- client/src/components/RegisterForm/components/PasswordField.jsx
+++ client/src/components/RegisterForm/components/PasswordField.jsx
@@ -1,20 +1,39 @@
-import { useState } from 'react'
+const PasswordField = ({ state, setState, posError, messageError, regex, ...props }) => {
+  const handleChange = ({ target }) => setState((prev) => ({ ...prev, value: target.value }))
+  const validateField = () => {
+    if (!regex) return
+    const validated = regex.test(state.value)
+    setState((prev) => ({ ...prev, validated }))
+  }
 
-const PasswordField = ({ ...props }) => {
-  const [type, setType] = useState('password')
+  const changeType = () => {
+    if (state.type === 'text') setState((prev) => ({ ...prev, type: 'password' }))
+    else if (state.type === 'password') setState((prev) => ({ ...prev, type: 'text' }))
+  }
+
+  const handleBlur = () => {
+    validateField()
+    changeType()
+  }
+
   return (
     <div className="register-form__input-wrapper">
       <div className="register-form__input-container">
         <input
           {...props}
-          type={type}
-          className="register-form__input-element"
+          type={state.type}
+          value={state.value}
+          className={`register-form__input-element ${
+            state.validated === false ? 'register-form__error' : ''
+          }`}
           autoComplete="new-password"
-          onClick={() => setType('text')}
-          onFocus={() => setType('text')}
-          onBlur={() => setType('password')}
+          onChange={handleChange}
+          onKeyUp={validateField}
+          onFocus={changeType}
+          onBlur={handleBlur}
         />
       </div>
+      {state.validated === false && <span className="register-form__input-element-icon" />}
     </div>
   )
 }
