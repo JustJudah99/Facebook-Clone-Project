c7a3155a575f88847f8f8694948eb0792e6e9791
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-08 17:30:04 -0500

    fix(Client): Extract logic from form component

--- client/src/components/RegisterForm/index.jsx
+++ client/src/components/RegisterForm/index.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import { regexs } from '../../constants'
 import DateOfBirthField from './components/DateOfBirthField'
 import GenderField from './components/GenderField'
 import InputField from './components/InputField'
 import PoliciesSection from './components/PoliciesSection'
+import useRegister from './hooks/useRegister'
 import './styles.css'
 
 const RegisterForm = ({ handleClose }) => {
-  const [name, setName] = useState({ type: 'text', value: '', validated: null })
-  const [lastname, setLastName] = useState({ type: 'text', value: '', validated: null })
-  const [email, setEmail] = useState({ type: 'email', value: '', validated: null })
-  const [password, setPassword] = useState({ type: 'password', value: '', validated: null })
-
+  const { fields, handleSubmit } = useRegister()
   return (
     <div className="register-form__wrapper">
       <div className="register-form__container">
@@ -27,49 +22,13 @@ const RegisterForm = ({ handleClose }) => {
           <h3 className="register-form__title">Registrarte</h3>
           <p className="register-form__subtitle">Es rápido y fácil.</p>
         </header>
-        <form
-          className="register-form__form"
-          onSubmit={(e) => {
-            e.preventDefault()
-          }}
-        >
+        <form className="register-form__form" onSubmit={handleSubmit}>
           <section className="register-form__section-fields">
-            <InputField
-              state={name}
-              setState={setName}
-              posError="left"
-              messageError="¿Cómo te llamas?"
-              regex={regexs.name}
-              placeholder="Nombre"
-            />
-            <InputField
-              state={lastname}
-              setState={setLastName}
-              posError="bottom"
-              messageError="¿Cómo te llamas?"
-              regex={regexs.name}
-              placeholder="Apellido"
-            />
+            <InputField {...fields.name} />
+            <InputField {...fields.lastname} />
           </section>
-          <InputField
-            state={email}
-            setState={setEmail}
-            posError="left"
-            messageError="Ingresa un número de teléfono celular o una dirección de correo electrónico válidos."
-            regex={regexs.email}
-            placeholder="Número de celular o correo electrónico"
-          />
-          <InputField
-            state={password}
-            setState={setPassword}
-            posError="left"
-            messageError="Ingresa una combinación de al menos seis números, letras y signos de puntuación (como ! y &)."
-            regex={regexs.password}
-            placeholder="Contraseña nueva"
-            onClick={() => setPassword((prev) => ({ ...prev, type: 'text' }))}
-            onFocus={() => setPassword((prev) => ({ ...prev, type: 'text' }))}
-            onBlur={() => setPassword((prev) => ({ ...prev, type: 'password' }))}
-          />
+          <InputField {...fields.email} />
+          <InputField {...fields.password} />
           <DateOfBirthField />
           <GenderField />
           <PoliciesSection />
