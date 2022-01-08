bf9765e5b9c874b3534b8dc2a7f909fac61f04e6
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-07 19:35:01 -0500

    feat(Client): Use radiobutton GenderField component

--- client/src/components/RegisterForm/components/GenderField.jsx
+++ client/src/components/RegisterForm/components/GenderField.jsx
@@ -1,22 +1,39 @@
+import { useState } from 'react'
+import { genders, pronouns } from '../../../constants'
 import ContainerField from './ContainerField'
+import InputField from './InputField'
+import RadioButton from './RadioButton'
+import SelectionField from './SelectionField'
+
+const NOT_SELECT_GENDER = 0
+const OTHER_GENDER_SELECTION = genders.find(({ label }) => label === 'Personalizado').value || 3
 
 const GenderField = () => {
+  const [state, setState] = useState({ gender: NOT_SELECT_GENDER, otherGender: 0 })
+
+  const changeGender = ({ target }) => setState((prev) => ({ ...prev, gender: +target.value }))
+  const changeOther = ({ target }) => setState((prev) => ({ ...prev, otherGender: +target.value }))
+
   return (
     <ContainerField label="Sexo">
       <div className="register-form__wrapper-select">
-        <span className="register-form__checkbox">
-          <label htmlFor="radio-sex-woman">Mujer</label>
-          <input type="radio" name="sex" id="radio-sex-woman" />
-        </span>
-        <span className="register-form__checkbox">
-          <label htmlFor="radio-sex-man">Hombre</label>
-          <input type="radio" name="sex" id="radio-sex-man" />
-        </span>
-        <span className="register-form__checkbox">
-          <label htmlFor="radio-sex-p">Personalizado</label>
-          <input type="radio" name="sex" id="radio-sex-p" />
-        </span>
+        {genders.map((gender) => (
+          <RadioButton
+            key={gender.label}
+            state={state.gender}
+            handleChange={changeGender}
+            {...gender}
+          />
+        ))}
       </div>
+      {state.gender === OTHER_GENDER_SELECTION && (
+        <div className="register-form__wrapper-select--margin">
+          <SelectionField options={pronouns} value={state.other} onChange={changeOther} />
+          <ContainerField label="Tu pronombre será visible para todos." activeBtn={false}>
+            <InputField placeholder="Género (opcional)" />
+          </ContainerField>
+        </div>
+      )}
     </ContainerField>
   )
 }
