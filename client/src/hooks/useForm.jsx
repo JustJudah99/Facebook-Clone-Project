2701c98d6708dd5e4c06975476b66a4d6a1e9c4a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-09 16:46:52 -0500

    feat(Client): Create custom hook useForm

---
+++ client/src/hooks/useForm.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+export default function useForm(initialValues, validateValues) {
+  const [values, setValues] = useState(initialValues)
+  const [touched, setTouched] = useState([])
+  const [errors, setErrors] = useState({})
+  const [state, setState] = useState({ loading: false, response: null })
+
+  const checkTouchedFields = (errors) => {
+    const newErrors = { ...errors }
+    Object.keys(newErrors).forEach((input) => {
+      if (!touched.includes(input)) delete newErrors[input]
+    })
+    return newErrors
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setValues((prev) => ({ ...prev, [name]: value }))
+    setTouched((prev) => [...prev, name])
+  }
+
+  const handleBlur = (event) => {
+    handleChange(event)
+    setErrors(validateValues(values))
+  }
+
+  const handleFocus = (event) => {
+    const { name } = event.target
+    console.log({ name })
+    setErrors((prev) => {
+      const newErrors = { ...prev }
+      Object.keys(newErrors).forEach((input) => {
+        if (input === name) delete newErrors[input]
+      })
+      return newErrors
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setErrors(validateValues(values))
+
+    if (Object.keys(errors).length !== 0) return
+    setState({ loading: false, response: null })
+  }
+
+  return {
+    values,
+    errors: checkTouchedFields(errors),
+    isLoading: state.loading,
+    response: state.response,
+    handleChange,
+    handleBlur,
+    handleFocus,
+    handleSubmit
+  }
+}
