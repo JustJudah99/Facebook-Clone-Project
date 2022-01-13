8239ad4c9f6ef4fdcee6d1d3409cbf44cdd8a24e
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-01-12 23:14:15 -0500

    feat(Client): Use privateRoute component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,17 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { UIContextProvider } from './context/UIContext'
-import { UserContextProvider } from './context/UserContext'
-import Home from './pages/Home'
+import { PrivateRoute } from './components/Routes/PrivateRoute'
+import UserHome from './pages/UserHome'
 
 function App() {
   return (
-    <UserContextProvider>
-      <UIContextProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </BrowserRouter>
-      </UIContextProvider>
-    </UserContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/" element={<UserHome />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,25 @@
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  HttpLink
-} from "@apollo/client";
+import { UIContextProvider } from './context/UIContext'
+import { UserContextProvider } from './context/UserContext'
+import './index.css'
 
 const client = new ApolloClient({
   link: new HttpLink({
     uri: 'http://localhost:8000/'
   }),
   cache: new InMemoryCache()
-});
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <UserContextProvider>
+      <UIContextProvider>
+        <App />
+      </UIContextProvider>
+    </UserContextProvider>
   </ApolloProvider>,
   document.getElementById('root')
 )
